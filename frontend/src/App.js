345c0b2fe36ac44cd7aed0da5e40ca7fe7247b8c
bolortuka45-max <bolortuka45@gmail.com>
2025-08-17 20:28:50 +0800

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./LoginPage";
+import SelectionPage from "./SelectionPage";
 
 function App() {
-  return <LoginPage />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/selection" element={<SelectionPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
